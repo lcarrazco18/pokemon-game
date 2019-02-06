d128e5da3c22c3017cb481044e5c9e0e58412b19
luiscarrazco18 <luiscarrazco@gmail.com>
2019-02-06 09:40:13 -0700

    cpuPick

--- index.js
+++ index.js
@@ -8,21 +8,23 @@ console.log(pokemonsEL)
 var i = 0;
 
 while (i < pokemonsEL.length) {
-  pokemonsEL[i].onclick = function() {
+  pokemonsEL[i].onclick = function () {
     var pokemonName = this.dataset.pokemon
     gameState.userPokemon = pokemonName
 
+    cpuPick()
     console.log(gameState)
-  
+
   }
   i++
 }
 
 function randomNumber(min, max) {
-  
+  return Math.floor(Math.random() * (max - min)) + min;
 }
-function cpuPick() {
 
+function cpuPick() {
+  gameState.rivalPokemon = pokemonsEL[randomNumber(0, 3)].dataset.pokemon
 }
 
 
@@ -65,33 +67,31 @@ function cpuPick() {
 //       stamina: 39,
 //       level: 1
 //     },
-    
+
 //   ]
-  
-  
+
+
 //   var attack = 20;
 //   var level = 10;
 //   var stack = 1.3;
 //   var stamina = 39;
-  
+
 //   // create a formula for attacks
 //   console.log((attack * level ) * stack / 7)
-  
-  
-  
+
+
+
 //   // create a formula for health
 //   //HP = 0.20 x Sqrt(Pokemon_level) x (HP_base_stat)
 //   console.log(((0.20 * Math.sqrt(level)) * stamina) * 15)
-  
-  
-  
-  
+
+
+
+
 //   // let user choose 1 and then assign a random pokemon to battle thats not the users pokemon
 //   // p1 vs p2
-  
-  
-  
-  
+
+
+
+
 //   // when one user loses all his health declare a winner
-  
-  
